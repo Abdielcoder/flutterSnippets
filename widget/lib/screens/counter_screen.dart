d8fa97c13e2e81819da967d7fff17adcc8838233
Abdiel Carrasco <dev@abdiel.me>
2023-01-02 15:07:02 -0800

    SNP03 - ADDING NEW PROJECT

--- widget/lib/screens/counter_screen.dart
+++ widget/lib/screens/counter_screen.dart
@@ -6,22 +6,40 @@ class CounterScreen extends StatefulWidget {
   const CounterScreen({Key? key}) : super(key: key);
 
   @override
-  State<CounterScreen> createState() => _CounterScreenState();//Crate State for StatefulWidget
+  State<CounterScreen> createState() =>
+      _CounterScreenState(); //Crate State for StatefulWidget
 }
 
 class _CounterScreenState extends State<CounterScreen> {
-  int counter = 0;//Initialize on class scope
+  int counter = 0; //Initialize on class scope
+  //FUNCTIONS TO CALL STATES TO BUTTONS
+  void increase() {
+    counter++;
+    setState(() {});
+  }
+
+  void decrease() {
+    counter--;
+    setState(() {});
+  }
+
+  void reset() {
+    counter = 0;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     // const fontSise40 = TextStyle(fontSize: 40);
 
     return Scaffold(
-      appBar: AppBar(title: const Text('Counter Screen')),
+     // appBar: AppBar(title: const Text('Counter Screen')),
+     appBar: MyAppBar.getAppBar('Counter Screen'),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           //crossAxisAlignment: CrossAxisAlignment.center, Aligment childrens
-          children:  <Widget>[
+          children: <Widget>[
             const Text(
               'Numero de Tabs',
               style: TextStyle(
@@ -29,7 +47,8 @@ class _CounterScreenState extends State<CounterScreen> {
                   fontWeight: FontWeight.bold,
                   fontSize: 40),
             ),
-            Text('$counter',
+            Text(
+              '$counter',
               style: const TextStyle(
                   color: Colors.indigo,
                   fontWeight: FontWeight.bold,
@@ -39,20 +58,55 @@ class _CounterScreenState extends State<CounterScreen> {
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: FloatingActionButton(
-          child: const Icon(Icons.rocket),
-          onPressed: () {
-            counter++;//Increment Counter
-            setState(() {//This is a anonymus function
-
-            });
-          },
-        ),
-      ),
+      floatingActionButton: flotatButton(
+          //CALL THE REFERENCE OF FUCTIONS
+          increaseFn: increase,
+          decreaseFn: decrease,
+          resetFn: reset),
     );
 
     throw UnimplementedError();
+  }
+}
+
+class flotatButton extends StatelessWidget {
+  //DECLARE FUNCTIONS
+  final Function increaseFn;
+  final Function decreaseFn;
+  final Function resetFn;
+
+  const flotatButton(
+      {Key? key,
+      //PASS REFERENCE TO CONSTRUCTOR
+      required this.increaseFn,
+      required this.decreaseFn,
+      required this.resetFn})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Padding(
+        padding: const EdgeInsets.only(bottom: 10, right: 10),
+        child: FloatingActionButton(
+          child: const Icon(Icons.rocket),
+          onPressed: () => increaseFn(), //CALL FUNCTION WIDTH DE REFERENCE
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.only(bottom: 10, right: 10),
+        child: FloatingActionButton(
+          child: const Icon(Icons.expand_more),
+          onPressed: () => decreaseFn(), //CALL FUNCTION WIDTH DE REFERENCE
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.only(bottom: 10, right: 10),
+        child: FloatingActionButton(
+          child: const Icon(Icons.refresh),
+          onPressed: () => resetFn(), //CALL FUNCTION WIDTH DE REFERENCE
+        ),
+      ),
+    ]);
   }
 }
--- widget/lib/utils/my_app_bar.dart
+++ widget/lib/utils/my_app_bar.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:new_gradient_app_bar/new_gradient_app_bar.dart';
 
-class MyAppBar extends StatelessWidget {
-  const MyAppBar({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
+class MyAppBar{
+  static getAppBar(String title) {
     return NewGradientAppBar(
-        title: Text('Flutter'),
-        gradient:
-            LinearGradient(colors: [Colors.blue, Colors.purple, Colors.red]));
+      title: Text(title),
+        gradient: const LinearGradient(colors: [Colors.black, Colors.indigo, Colors.deepPurple])
+    );
   }
+
 }
